37367feeef5478da210242a4aa7c8e09cbbbae79
Audrey Pujol <audrey.pujol.machado@gmail.com>
2021-08-16 22:07:16 -0300

    Javascript Introdução e Intermediário

---
+++ aula1Js/condicionais2.js
@@ -0,0 +1,13 @@
+var nome = 'Tiago';
+
+switch (nome) {
+    case 'Audrey':
+        console.log('Legal! o seu nome é maravilhoso!');
+            break;
+        case 'Pujol':
+            console.log('Seu sobrenome também é maravilhoso!');
+                break;
+        default:
+            console.log('Que pena, você não é a maravilhosa da Audrey Pujol! Quem sabe um dia você chega lá!');
+            break;
+}
