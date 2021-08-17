16c884d7dcacbab9cd8b7c25ebb067bc163bdfe1
Audrey Pujol <audrey.pujol.machado@gmail.com>
2021-08-17 15:42:38 -0300

    Javascript Introdução e Intermediário

---
+++ aula1Js/arrowFunctions.js
@@ -0,0 +1,18 @@
+function soma(x, y) {
+    return x + y;
+}
+
+const multiplicacao = function (x, y) {
+    return x * y;
+};
+
+const dividir = (x, y) => {
+    return x / y;
+};
+
+
+const objeto = () => ({ nome: 'Audrey', sobrenome: 'Pujol' });
+
+console.log(objeto);
+
+
---
+++ aula1Js/operadorTernario.js
@@ -0,0 +1,23 @@
+let isValid = true;
+
+// Sem Ternário
+
+function verify(isValid) {
+    if(isValid) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(verify(isValid));
+
+// Com Ternário
+
+const result = isValid ? true : false;
+
+let zero = 0;
+
+const numeroResultado = zero > 0 ? 0 : -1;
+
+console.log(numeroResultado);
