b83e223e72e10bf914d6f361f1d8d3407c5c92f1
Audrey Pujol <audrey.pujol.machado@gmail.com>
2021-08-17 14:58:04 -0300

    Javascript Introdução e Intermediário

---
+++ aula1Js/templateString.js
@@ -0,0 +1,20 @@
+let meuNome = 'Audrey';
+let meuSobrenome = 'Pujol';
+let minhaProfissao = 'Backend Developer';
+
+// Sem Template String
+console.log(
+    'Olá, eu sou ' + 
+        meuNome + 
+        ' ' + 
+        meuSobrenome + 
+        ' e minha profissão é: ' + 
+        minhaProfissao
+);
+
+// Com Template String
+
+console.log(`Olá, eu sou ${meuNome} ${meuSobrenome} e minha profissão é: ${minhaProfissao}`
+);
+
+console.log(`O resultado da soma de 1 + 1 é ${1 + 1}`);
