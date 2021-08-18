191077e18623192af83b231bcc60674665232af7
Audrey Pujol <audrey.pujol.machado@gmail.com>
2021-08-18 14:21:00 -0300

    Javascript Introdução e Intermediário

---
+++ aula1Js/desestruct.js
@@ -0,0 +1,13 @@
+const pessoa = {
+    nome: 'Audrey',
+    sobrenome: 'Pujol',
+    idade: 32,
+    profissao: 'Backend Developer'
+};
+
+console.log(pessoa);
+
+
+let { nome, sobrenome, idade, profissao } = pessoa;
+
+console.log(nome, sobrenome, idade, profissao);
